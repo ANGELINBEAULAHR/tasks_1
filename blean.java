59a23092070caa8e802dbd3da7dbbeb07a272cd8
ANGELINBEAULAHR <angelinbeaulahr71@gmail.com>
2026-06-05 23:37:47 +0530

    new file

---
+++ blean.java
@@ -0,0 +1,14 @@
+
+public class blean {
+    public static void main(String[] args){
+        blean b = new blean();
+        if(true){
+           System.out.println("yes");
+        }
+           System.out.println(b.trial());
+    }
+    protected String m ="yes";
+    public String trial(){
+        return m;
+    }
+}
